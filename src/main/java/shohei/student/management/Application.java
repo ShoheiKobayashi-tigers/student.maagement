4fc688e4736e834b470126b6886a1b97ab13ca04
ShoheiKobayashi <koba.5387.tigers@gmail.com>
2025-04-15 21:53:58 +0900

    2ndcommit

--- src/main/java/shohei/student/management/Application.java
+++ src/main/java/shohei/student/management/Application.java
@@ -3,6 +3,7 @@ package shohei.student.management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -10,14 +11,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class Application {
 
+  private String name = "Shohei Kobayashi";
+  private String age = "25";
+
+
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
 
-  @GetMapping("/hello")
-  public String hello() {
+  @GetMapping("/studentInfo")
+  public String getstudentInfo() {
+    return name + " " + age + "歳";
+  }
 
-    return "Hello world!!!!!!!!!!!!!!!!!";
+  @PostMapping("/studentInfo")
+  public void setstudentInfo(String name, String age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  @PostMapping("/studentName")
+  public void updateStudentName(String name) {
+    this.name = name;
   }
 
 }
