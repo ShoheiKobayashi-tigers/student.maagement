84cd37c2f0d9009986cab8765d2e7f979c7cd0cb
ShoheiKobayashi <koba.5387.tigers@gmail.com>
2025-04-18 18:45:58 +0900

    23_MyBatisを使ってCRUDのRead処理を実装　課題

--- src/main/java/shohei/student/management/Student.java
+++ src/main/java/shohei/student/management/Student.java
@@ -1,26 +1,20 @@
 package shohei.student.management;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Student {
 
+  private String id;
   private String name;
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
-  }
-
+  private String furigana;
+  private String nickname;
+  private String mail;
+  private String city;
   private int age;
+  private String gender;
 
 
 }
--- src/main/java/shohei/student/management/StudentRepository.java
+++ src/main/java/shohei/student/management/StudentRepository.java
@@ -1,28 +1,17 @@
 package shohei.student.management;
 
 import java.util.List;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface StudentRepository {
 
-  @Select("SELECT * FROM student WHERE name= #{name}")
-  Student searchByName(String name);
+  @Select("SELECT * FROM students")
+  List<Student> search();
 
-  @Select("SELECT * FROM student")
-  List<Student> allName();
+  @Select("SELECT * FROM students_courses")
+  List<Courses> searchCourses();
 
-  @Insert("INSERT student values(#{name},#{age})")
-  void registorStudent(String name, int age);
-
-  @Update("UPDATE student SET age=#{age} WHERE name=#{name}")
-  void updateStudentName(String name, int age);
-
-  @Delete("DELETE FROM student WHERE name=#{name}")
-  void deleteStudent(String name);
 
 }
