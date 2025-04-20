7437c9e31db72cd6cb2cdfffe23f103ad49cd126
ShoheiKobayashi <koba.5387.tigers@gmail.com>
2025-04-20 09:10:20 +0900

    24_Read処理のServiceとController部分を実装　課題

--- src/main/java/shohei/student/management/repository/StudentRepository.java
+++ src/main/java/shohei/student/management/repository/StudentRepository.java
@@ -6,8 +6,20 @@ import org.apache.ibatis.annotations.Select;
 import shohei.student.management.data.Courses;
 import shohei.student.management.data.Student;
 
+/**
+ * 受講生情報を扱うリポジトリ。
+ * <p>
+ * 全件検索や単一条件での検索、コース情報の検索が行えるクラスです。
+ */
+
 @Mapper
 public interface StudentRepository {
+
+  /**
+   * 全件検索をします。
+   *
+   * @return 全件検索した受講生情報の一覧。
+   */
 
   @Select("SELECT * FROM students")
   List<Student> search();
